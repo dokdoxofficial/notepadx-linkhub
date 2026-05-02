adcf3ce7ee2758561c10ca2ed14c2f735db700d3
Andy <dokdoxisgreat@gmail.com>
2026-05-02 21:44:30 +0900

    em

---
+++ index.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.title = "NotepadX LinkHub"
+
+st.link_button("📝NotepadX 메모장", "https://notepadx.netlify.app")
+st.link_button("📁 엑셀 정리", "https://...")
+st.link_button("🎥 유튜브 다운", "https://...")
